d97d5c124e4a7339505cf749d9f1e0fc175696b7
Kristof Kalai <kristof.kalai@supercharge.io>
2023-05-21 01:00:58 +0200

    feat: add example app

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Kristóf Kálai on 2023. 05. 21..
+//
+
+import SwiftUI
+import VisualEffectBlur
+
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.red, .blue]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+
+            VisualEffectBlur(blurStyle: .systemUltraThinMaterial, vibrancyStyle: .fill) {
+                Text("Hello World!")
+                    .font(.largeTitle)
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
