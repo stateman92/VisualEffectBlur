5cdf4ffca9bb015b3dced1b3ffed41bf0882af15
Kristof Kalai <kristof.kalai@supercharge.io>
2023-05-21 00:45:07 +0200

    refactor: refactor old codebase

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.2
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "VisualEffects",
+    name: "VisualEffectBlur",
     platforms: [
         .macOS(.v10_15),
         .iOS(.v13)
@@ -12,15 +12,21 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "VisualEffects",
-            targets: ["VisualEffects"]),
+            name: "VisualEffectBlur",
+            targets: ["VisualEffectBlur"]),
     ],
     dependencies: [
-
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "VisualEffects",
-            dependencies: [])
+            name: "VisualEffectBlur",
+            dependencies: []),
+        .testTarget(
+            name: "VisualEffectBlurTests",
+            dependencies: ["VisualEffectBlur"]),
     ]
 )
